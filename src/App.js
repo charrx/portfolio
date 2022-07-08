691808dbba17a748a5a4fb0ddb77430cc7cdfa22
charrx <leanygaard97@gmail.com>
2022-07-08 23:42:17 +0200

    adds 4 nav items

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./components/header/Header";
 function App() {
   return (
     <div className={styles.app}>
-      <h1 className={styles.header}>I am the boss</h1>
       <Header></Header>
     </div>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,8 +2,21 @@ import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className={styles.header}>Hello World</h1>
+    <div className={styles.nav_bar_container}>
+      <div className={styles.nav_item_container}>
+        <p>
+          <span className={styles.number}>01.</span> Me
+        </p>
+        <p>
+          <span className={styles.number}>02.</span> Experience
+        </p>
+        <p>
+          <span className={styles.number}>03.</span> Projects
+        </p>
+        <p>
+          <span className={styles.number}>04.</span> Contact
+        </p>
+      </div>
     </div>
   );
 };
